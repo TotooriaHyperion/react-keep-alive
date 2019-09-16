799974f8768abac201f9b86ddb20a2f617af6b05
TotooriaHyperion <shr9192@gmail.com>
2019-09-16 20:00:44 +0800

    ssr-example

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,17 +1,13 @@
-import React, {useState} from 'react';
-import ReactDOM from 'react-dom';
-import {
-  Switch,
-  Route,
-  Link,
-  BrowserRouter as Router,
-} from 'react-router-dom';
-import { Provider as ReduxProvider } from 'react-redux';
-import { createStore } from 'redux';
-import {Provider, KeepAlive} from '../../es';
-import A from './views/A';
-import B from './views/B';
-import C from './views/C';
+import React, { useState, useEffect, useMemo } from "react";
+import ReactDOM from "react-dom";
+import ReactDOMServer from "react-dom/server";
+import { Switch, Route, Link, BrowserRouter as Router } from "react-router-dom";
+import { Provider as ReduxProvider } from "react-redux";
+import { createStore } from "redux";
+import { Provider, KeepAlive } from "../../es";
+import A from "./views/A";
+import B from "./views/B";
+import C from "./views/C";
 
 function App() {
   const [toggle, setToggle] = useState(true);
@@ -30,7 +26,9 @@ function App() {
       </ul>
 
       <div>
-        <button onClick={() => setToggle(!toggle)}>toggle({toggle.toString()})</button>
+        <button onClick={() => setToggle(!toggle)}>
+          toggle({toggle.toString()})
+        </button>
       </div>
 
       <Switch>
@@ -45,7 +43,10 @@ function App() {
         <Route
           path="/b"
           render={props => (
-            <KeepAlive name="A" extra={props}><B /><B /></KeepAlive>
+            <KeepAlive name="A" extra={props}>
+              <B />
+              <B />
+            </KeepAlive>
           )}
         />
         <Route
@@ -63,24 +64,74 @@ function App() {
 
 const store = createStore(function counter(state = 0, action) {
   switch (action.type) {
-  case 'INCREMENT':
-    return state + 1;
-  case 'DECREMENT':
-    return state - 1;
-  default:
-    return state;
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+    default:
+      return state;
   }
 });
 
 ReactDOM.render(
-  (
+  <ReduxProvider store={store}>
+    <Router>
+      <Provider>
+        <App />
+      </Provider>
+    </Router>
+  </ReduxProvider>,
+  document.getElementById("root"),
+);
+
+console.log(
+  "no ssr content",
+  ReactDOMServer.renderToString(
     <ReduxProvider store={store}>
       <Router>
         <Provider>
           <App />
         </Provider>
       </Router>
-    </ReduxProvider>
+    </ReduxProvider>,
   ),
-  document.getElementById('root')
 );
+
+function useReady() {
+  const [ready, setReady] = useState(false);
+  useEffect(() => {
+    setReady(true);
+  }, []);
+  return ready;
+}
+const Example = () => {
+  const isReady = useReady();
+  const [name, setName] = useState("Hello");
+  const child = useMemo(() => {
+    return <div>{name}</div>;
+  }, [name]);
+  return (
+    <div>
+      <button
+        onClick={() => setName(old => (old === "Hello" ? "world" : "Hello"))}
+      >
+        Toggle
+      </button>
+      <Provider>
+        {!isReady ? (
+          child
+        ) : (
+          <KeepAlive name={name} key={name}>
+            {child}
+          </KeepAlive>
+        )}
+      </Provider>
+    </div>
+  );
+};
+
+const domStr = ReactDOMServer.renderToString(<Example></Example>);
+console.log("render immediate at first time", domStr);
+document.querySelector(".example").innerHTML = domStr;
+
+ReactDOM.hydrate(<Example></Example>, document.querySelector(".example"));
